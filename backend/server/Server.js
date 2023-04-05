625f7a89bc59186d23d4d7631436dda65791dcfa
Ashu313 <79858134+Ashu313@users.noreply.github.com>
2023-04-05 13:35:23 +0530

    port changes

---
+++ backend/server/Server.js
@@ -0,0 +1,26 @@
+ 
+const cors=require('cors')
+const express=require('express');
+const app=express();
+app.use(cors());
+
+const dbConnect=require('../server/database/database');
+const getTickersData = require('./controllers/getTickers');
+const dotenv=require('dotenv');
+dotenv.config();
+const PORT=process.env.PORT||5000;
+
+dbConnect();
+
+const port=5000;
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+  });
+
+  app.get('/tickers',getTickersData);
+
+  app.listen(port, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+
