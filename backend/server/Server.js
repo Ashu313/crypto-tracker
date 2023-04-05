4b1d9503874df3a97748dd2ab3f72185804ea2fa
Ashu313 <79858134+Ashu313@users.noreply.github.com>
2023-04-05 14:50:26 +0530

    changes

--- backend/server/Server.js
+++ backend/server/Server.js
@@ -1,28 +1,10 @@
- 
-
-const express=require('express');
-const app=express();
-const cors=require('cors')
-
-app.use(cors());
-
-const dbConnect=require('../server/database/database');
-const getTickersData = require('./controllers/getTickers');
-const dotenv=require('dotenv');
-dotenv.config();
+const http=require('http');
 const PORT=process.env.PORT||5000;
-
+const app=require('./app.');
+const dbConnect=require("./database/database");
 dbConnect();
+const server=http.createServer(app);
+ 
+server.listen(PORT,console.log(`server is running at ${PORT}`));
 
-const port=5000;
-app.use(express.json());
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-  });
-
-  app.get('/tickers',getTickersData);
-
-  app.listen(port, () => {
-    console.log(`Server started on port ${PORT}`);
-  });
-
+//mongodb+srv://ashu:<password>@expense.wsn4oy6.mongodb.net/?retryWrites=true&w=majority
---
+++ backend/server/app..js
@@ -0,0 +1,25 @@
+ 
+
+const express=require('express');
+const app=express();
+const cors=require('cors')
+
+app.use(cors());
+
+const dbConnect=require('./database/database')
+const getTickersData = require('./controllers/getTickers');
+const dotenv=require('dotenv');
+dotenv.config();
+
+
+dbConnect();
+
+
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+  });
+
+  app.get('/tickers',getTickersData);
+
+
