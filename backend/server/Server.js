040f1f953f85d1c2ba7a9b88b0ada7064fe283ef
Ashu313 <79858134+Ashu313@users.noreply.github.com>
2023-04-05 14:12:22 +0530

    change

--- backend/server/Server.js
+++ backend/server/Server.js
@@ -1,8 +1,9 @@
  
 
 const express=require('express');
-const cors=require('cors')
 const app=express();
+const cors=require('cors')
+
 app.use(cors());
 
 const dbConnect=require('../server/database/database');
